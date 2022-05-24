a58c9963f3c3d4a9fcab138008dfece3d3aa61cc
Somen Singha <somensingha06@gmail.com>
2022-05-24 21:15:02 +0600

    added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -28,6 +28,13 @@ async function run() {
       //   const cursor = productCollection.find(query);
       const products = await productCollection.find().toArray();
       res.send(products);
+    });
+
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
     });
   } finally {
     // Ensures that the client will close when you finish/error
